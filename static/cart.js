4455d0e1ccb8f714ba33d8c58f724eba8063ab0d
Eric Burden <eric@brothersofgrey.net>
2023-11-01 21:54:36 +0100

    Checks to get warenkorb (WIP)

--- static/cart.js
+++ static/cart.js
@@ -1,10 +1,21 @@
 // warenkorb array declaration
-const warenkorb = [];
+if (typeof warenkorb === "undefined" || warenkorb === null) {
+  warenkorb = []
+}
+
 
 function showCart() {
     //warenkorb = "{47: 1, 13: 7}";
-    wtable = document.getElementById("warenkorb").innerHTML;
-    document.write(wtable);
+    //wtable = document.getElementById("warenkorb").innerHTML;
+    //document.write(wtable);
+    var ausgabe = '';
+    for (var i = 0; i < warenkorb.length; ++i) {
+        ausgabe += '<li id="nr' + i + '">';
+        ausgabe += warenkorb[i];
+        ausgabe += '<input type="button" value="X" id="loeschen" onclick="delItem(' + i + ');" />';
+        ausgabe += '</li>';
+    }
+    document.getElementById('liste').innerHTML = ausgabe;
     /*
     <p id="warenkorb"></p>
     <button type="submit" onclick="delCart()" background-color: red>Alles löschen</button>
@@ -30,26 +41,33 @@ function submitCart() {
 
 function addArticle() {
     // Get the values of the input fields
-    let verkID = document.getElementById("verkID").value;
-    let betrag = document.getElementById("betrag").value;
+    // TODO: Try "valueAsNumber" instead of "value"
+    const verkID = document.getElementById("verkID").value;
+    const betrag = document.getElementById("betrag").value;
     
     console.log("DEBUG: Zuweisung: verkID=" + verkID + ", betrag=" + betrag);
 
     // Not a Number validation
-    let text;
     if (isNaN(parseInt(verkID)) || isNaN(parseFloat(betrag))) {
-      postMessage("Input not valid");
+      // TODO: Use "postMessage" for feedback to user
+      console.log("ERROR: Invalid Input");
     } else {
       //warenkorb += '{"verkID": verkID, "betrag": betrag}';
-      warenkorb.push("{verkID:" + verkID + ",betrag:" + betrag +"}");
+      console.log("DEBUG: Warenkorb Items: verkID: " + verkID + ", betrag: " + betrag);
+      warenkorb.push("{verkID:" + verkID + ", betrag:" + betrag + "}");
     }
-    console.log("DEBUG: Warenkorb Items: verkID: " + verkID + ", betrag: " + betrag);
     console.log("DEBUG: Warenkorb: " + warenkorb);
 
-    document.getElementById("warenkorb").innerHTML = warenkorb;
-
+    //document.getElementById("warenkorb").innerHTML = warenkorb;
+    //return warenkorb;
+    showCart();
 }
 
 function delCart() {
-    true;
+  true;
+}
+
+function delItem(id) {
+  warenkorb.splice(id, 1);
+  showCart();
 }
